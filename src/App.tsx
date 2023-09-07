9f9b7d0947a555c8ccc72a2a114fe1365c4ebab5
Delano Francis <deltel000@gmail.com>
2023-09-06 21:08:33 -0500

    Add buttons for protected and login route

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+async function login() {
+  const response = await fetch(
+    // `${process.env.REACT_APP_SERVER_URL_LIVE}/v1/login`,
+    `${process.env.REACT_APP_SERVER_URL_LOCAL}/v1/login`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify({
+        email: "anon@anon.com",
+        password: "password",
+      }),
+    }
+  );
+
+  localStorage.setItem(
+    "x-csrf-token",
+    response.headers.get("x-csrf-token") as string
+  );
+}
+
+async function getProtected() {
+  const xCsrfToken = localStorage.getItem("x-csrf-token") as string;
+  const response = await fetch(
+    // `${process.env.REACT_APP_SERVER_URL_LIVE}/v1/protected`,
+    `${process.env.REACT_APP_SERVER_URL_LOCAL}/v1/protected`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "X-CSRF-Token": xCsrfToken,
+      },
+      credentials: "include",
+    }
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={login}>Login</button>
+      <button onClick={getProtected}>Protected Route</button>
     </div>
   );
 }
