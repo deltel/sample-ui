bd359d39292f327298b89e28b8953b41c0b03810
Delano Francis <deltel000@gmail.com>
2023-10-26 20:24:20 -0500

    Update authentication state

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,9 @@
-async function login() {
-  const response = await fetch(
-    // `${process.env.REACT_APP_SERVER_URL_LIVE}/v1/login`,
-    `${process.env.REACT_APP_SERVER_URL_LOCAL}/v1/login`,
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      credentials: "include",
-      body: JSON.stringify({
-        email: "anon@anon.com",
-        password: "password",
-      }),
-    }
-  );
-
-  localStorage.setItem(
-    "x-csrf-token",
-    response.headers.get("x-csrf-token") as string
-  );
-}
+import { useEffect, useState } from "react";
 
 async function getProtected() {
+  console.log("protected");
   const xCsrfToken = localStorage.getItem("x-csrf-token") as string;
+  console.log("x-csrf-token", xCsrfToken);
   const response = await fetch(
     // `${process.env.REACT_APP_SERVER_URL_LIVE}/v1/protected`,
     `${process.env.REACT_APP_SERVER_URL_LOCAL}/v1/protected`,
@@ -35,12 +16,74 @@ async function getProtected() {
       credentials: "include",
     }
   );
+
+  console.log("response", response);
+  console.log("response headers", response.headers);
+  console.log(
+    "response headers > x-csrf-token",
+    response.headers.get("x-csrf-token")
+  );
+  const responseJson = await response.json();
+  console.log("json response", responseJson);
 }
 
 function App() {
+  const [authenticated, setAuthenticated] = useState(false);
+
+  useEffect(() => {
+    let loggedIn = localStorage.getItem("x-csrf-token");
+    if (loggedIn) {
+      setAuthenticated(true);
+    } else {
+      setAuthenticated(false);
+    }
+  }, [authenticated]);
+
+  async function login() {
+    console.log("login");
+    const response = await fetch(
+      // `${process.env.REACT_APP_SERVER_URL_LIVE}/v1/login`,
+      `${process.env.REACT_APP_SERVER_URL_LOCAL}/v1/login`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
+        body: JSON.stringify({
+          email: "anon@anon.com",
+          password: "password",
+        }),
+      }
+    );
+
+    console.log("response", response);
+    console.log(
+      "response headers - x-csrf-token >",
+      response.headers.get("x-csrf-token")
+    );
+    localStorage.setItem(
+      "x-csrf-token",
+      response.headers.get("x-csrf-token") as string
+    );
+    const responseJson = await response.json();
+    console.log("json response", responseJson);
+    setAuthenticated(true);
+  }
+
+  function logoutV2() {
+    localStorage.removeItem("x-csrf-token");
+    setAuthenticated(false);
+  }
+
   return (
     <div className="App">
-      <button onClick={login}>Login</button>
+      {authenticated ? (
+        <button onClick={logoutV2}>Log out</button>
+      ) : (
+        <button onClick={login}>Login</button>
+      )}
+
       <button onClick={getProtected}>Protected Route</button>
     </div>
   );
